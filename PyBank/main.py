91ee864ec5bd4b1a6de7de5395f7d4eaa84320b2
gw-sc <111397774+gw-sc@users.noreply.github.com>
2022-09-19 13:44:31 +0100

    update pyBank main

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,14 +7,14 @@ import csv
 
 # set variables
 months = []
-profloss = []
+profitloss = []
 
 month_count = []
-net_profloss = 0
-profloss_ch = []
-sum_profloss_ch = 0
-incr = 0
-decr = 0
+total_profitloss = 0
+profitloss_change = []
+sum_profitloss_change = 0
+greatest_increase = 0
+greatest_decrease = 0
 
 csvpath = os.path.join('PyBank', 'Resources', 'budget_data.csv')
 # csvpath = "./Resources/budget_data.csv"
@@ -35,46 +35,45 @@ with open(csvpath, 'r') as csvfile:
         if row[0] not in months:
             months.append(row[0])
             month_count = len(months)
-    # print(month_count)
+    print(month_count)
 
 # The net total amount of "Profit/Losses" over the entire period
-profloss.append(float(row[1]))
-
-for p in profloss:
-    net_profloss = net_profloss + p
-print(net_profloss)
+    profitloss.append(float(row[1]))
+    for p in profitloss:
+        total_profitloss = total_profitloss + p
+    print(total_profitloss)
 
 # # The changes in "Profit/Losses" over the entire period, and then the average of those changes
-# for i in range(len(profloss)-1):
-#     profloss_ch.append(profloss[i + 1] - profloss[i])
-# print(profloss_ch)
+# for i in range(len(profitloss)-1):
+#     profitloss_change.append(profitloss[i + 1] - profitloss[i])
+# print(profitloss_change)
 
-# for p in profloss_ch:
-#     sum_profloss_ch = sum_profloss_ch + p
-# average_profloss_ch = sum_profloss_ch / len(profloss_ch)
+# # for p in profloss_ch:
+# sum_profitloss_change = sum_profitloss_change + p
+# average_profitloss_change = sum_profitloss_change / len(profitloss_change)
 
 # # The greatest increase in profits (date and amount) over the entire period
-# for p in profloss_ch:
-#     if p > incr:
-#         incr = p
+# for p in profitloss_change:
+#     if p > greatest_increase:
+#         greatest_increase = p
 # print(p)
 
 # # The greatest decrease in profits (date and amount) over the entire period
-# for p in profloss_ch:
-#     if p > decr:
-#         decr = p
+# for p in profitloss_change:
+#     if p > greatest_decrease:
+#         greatest_decrease = p
 # print(p)
 
-# print to terminal
-# The 'analysis folder' contains your text file that has the results from your analysis.
+# # print to terminal
+# # The 'analysis folder' contains the text file that has the results from the analysis.
 # analysis = f'\
 # Financial Analysis\n\
 # ----------------------------\n\
 # Total Months: {month_count} \n\
-# Total Amount: ${profloss} \n\
-# Average Change: ${average_profloss_ch} \n\
-# Greatest Increase in Profits: ${incr} \n\
-# Greatest Decrease in Profits: ${decr} \n'
+# Total Amount: ${profitloss} \n\
+# Average Change: ${average_profitloss_change} \n\
+# Greatest Increase in Profits: ${greatest_increase} \n\
+# Greatest Decrease in Profits: ${greatest_decrease} \n'
 
 # print(analysis)
 
